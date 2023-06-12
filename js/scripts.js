8969dea41a6d81b982ea92b0e33f124d0414a173
Niwde36 <233edwin377@gmail.com>
2023-06-12 17:01:40 -0500

    Se agregan contacto, poemas, propósito y sus css, falta edición

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-// Toggle del menú hamburguesa
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', () => {
-menu.classList.toggle('active');
-});
